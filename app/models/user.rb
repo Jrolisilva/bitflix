7ca73d81723e3d2800bcd211f1f1b933ede1b598
Jrolisilva <jroliveira.sa@gmail.com>
2021-05-08 13:19:23 -0300

    Criando Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable, :validatable
 end
+
+
+
+
