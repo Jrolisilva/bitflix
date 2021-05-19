ebb5ae822f099e52dabb70bfe34c5ee3e34f5ce9
Jrolisilva <jroliveira.sa@gmail.com>
2021-05-19 16:57:34 -0300

    Configurando Vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
-import Vue from 'vue/dist/vue.esm'  
+import Vue from 'vue'
+  
 import App from './app.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import router from './routes.js'    
  
 Vue.use(Vuetify)
 
@@ -9,6 +11,7 @@ Vue.use(Vuetify)
 document.addEventListener('DOMContentLoaded', () => {
  const app = new Vue({
   el: '#app',
+  router,
   render: h => h(App)
  })
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "home#index"
+  match "path", to: 'home#index', via: :get
   
   namespace :api do
    namespace :v1 do
@@ -19,4 +20,5 @@ Rails.application.routes.draw do
     resources :recommendations, only: :index
    end
   end
+  match "path", to: "hone#index", via: :get
 end
