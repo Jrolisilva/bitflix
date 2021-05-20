3e636eaf756311b2817caec0a10f4a4d0e05de39
Jrolisilva <jroliveira.sa@gmail.com>
2021-05-20 17:35:32 -0300

    Configurando Vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,14 @@ import Vue from 'vue'
 import App from './app.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'slick-carousel/slick/slick.css';
 import router from './routes.js'    
  
 Vue.use(Vuetify)
 
- 
-document.addEventListener('DOMContentLoaded', () => {
- const app = new Vue({
+new Vue({
+  vuetify : new Vuetify(),
   el: '#app',
   router,
   render: h => h(App)
  })
-})
--- app/javascript/packs/components/home/index.vue
+++ app/javascript/packs/components/home/index.vue
@@ -1,13 +1,67 @@
 <template>
   <div>
-   <h4 class="white--text">Wellcome to HomePage</h4>
+   <Featured :movie="featuredMovie"/>
+    <MovieList v-for= '(category, index) in categories'
+              :key='index'
+              :name='category.name'
+              :movies='category.movies'/>
   </div>
 </template>
  
 <script>
+import Featured from './featured.vue';  
+import MovieList from './movie_list.vue';
+
+ // ------- Dados Fake apenas para testarmos o layout -------- //
+ const featuredMovie = {
+                         id: 1,
+                         title: 'Criando uma API completa com Ruby on Rails', 
+                         description: 'Saber como criar e consumirAPIs é fundamental para qualquer programador, então nessa pequena série nós vamos ver o que é essencial para criar uma usando RoR.',
+                         featured_thumbnail_url:'https://onebitcode.com/wp-content/uploads/2018/05/bg-example.png'
+                        }
+
+ const categoriesMock = [
+    {
+     name: "Ruby On Rails" , movies: [
+         {id: '1', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '2', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '3', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png' },
+         {id: '4', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '5', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '6' , thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+       ]
+     },{
+       name: "VueJS", movies: [
+       {id: '1' , thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+       {id: '2', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+       {id: '3', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+       {id: '4', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+       {id: '5', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+       {id: '6', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+       ]
+     },{
+       name: "Chatbots" , movies: [
+         {id: '1', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '2', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '3', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '4', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '5', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+         {id: '6', thumb_url:'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+       ]
+    }
+  ];
+ 
+ 
  export default {
   data () {
-   return { }
+   return { 
+      featuredMovie: featuredMovie,
+      categories: categoriesMock
+   }
+  },
+  components: {
+    'Featured' : Featured,
+    'MovieList': MovieList
   }
  }
 </script>
